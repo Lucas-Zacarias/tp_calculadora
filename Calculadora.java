78d9d430e30b1f44ee7cc3195dfa6c91fb619e49
Lucas-Zacarias <63613740+Lucas-Zacarias@users.noreply.github.com>
2020-04-15 16:45:52 -0300

    Create Calculadora.java

---
+++ Calculadora.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+ 
+
+public class Calculadora {
+public static void main(String[] args) {
+Integer a=0;
+Integer b=0;
+Integer opcion;
+Integer resultado = 0;
+
+ 
+
+Scanner teclado = new Scanner (System.in);
+
+ 
+
+do{
+System.out.println("Ingrese su operación"+ '\n'+ "1 para sumar" +'\n'+"2 pararestar "+'\n'+ "3 para multiplicar"+ '\n'+"4 para dividir");
+        
+opcion = teclado.nextInt();
+
+ 
+
+}while(opcion < 1 && opcion > 4);
+System.out.println("Ingrese el primer numero");
+a = teclado.nextInt();
+System.out.println("Ingrese el segundo numero");
+b = teclado.nextInt();
+
+ 
+
+if(opcion.equals(1)){
+resultado = a + b;
+}else if(opcion.equals(2)){
+resultado = a - b;
+}else if(opcion.equals(3)){
+resultado = a * b;
+}else{
+resultado = a / b;
+}
+System.out.println("El resultado es " + resultado.toString());
+}
+}
